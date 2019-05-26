e8a0ca470cb50d9e0ffbdf9df15f0cae3f2fa216
KentFujii <kent.where.the.light.is@gmail.com>
2019-05-27 00:47:10 +0900

    wip: getting_started

--- getting_started/src/main/scala/GettingStarted.scala
+++ getting_started/src/main/scala/GettingStarted.scala
@@ -6,12 +6,55 @@ object MyModule {
     else n
   }
 
-  private def formatAbs(x: Int) = {
-    val msg = "The absolute value of %d is %d"
-    msg.format(x, abs(x))
+  def factorial(n: Int): Int = {
+    def go(n: Int, acc: Int): Int = {
+      if (n <= 0) acc
+      else go(n-1, n*acc)
+    }
+    go(n, 1)
+  }
+
+  // private def formatAbs(x: Int) = {
+  //   val msg = "The absolute value of %d is %d"
+  //   msg.format(x, abs(x))
+  // }
+
+  // private def formatFactorial(n: Int) = {
+  //   val msg = "The factorial of %d is %d"
+  //   msg.format(n, factorial(n))
+  // }
+
+  def formatResult(name: String, n: Int, f: Int => Int) = {
+    val msg = "The %s of %d is %d"
+    msg.format(name, n, f(n))
+  }
+
+  // def findFirst(ss: Array[Int], key: Int): Int = {
+  //   @annotation.tailrec
+  //   def loop(n: Int): Int = {
+  //     if (n >= ss.length) -1
+  //     else if (ss(n) == key) n
+  //     else loop(n + 1)
+  //   }
+  //   loop(0)
+  // }
+
+  def findFirst[A](as: Array[A], p: A => Boolean): Int = {
+    @annotation.tailrec
+    def loop(n: Int): Int = {
+      if (n >= as.length) -1
+      else if (p(as(n))) n
+      else loop(n + 1)
+    }
+    loop(0)
   }
 
   def main(args: Array[String]): Unit = {
-    println(formatAbs(-42))
+    // println(formatAbs(-42))
+    // println(formatFactorial(7))
+    println(formatResult("absolute value", 42, abs))
+    println(formatResult("factorial value", 7, factorial))
+    // println(findFirst(Array(7, 9, 13), 13))
+    println(findFirst(Array(7, 9, 13), (x: Int) => x == 9))
   }
 }
