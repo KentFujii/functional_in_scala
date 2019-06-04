5ebfa6671e5be3256362d9696b9f05ebc767600e
KentFujii <kent.where.the.light.is@gmail.com>
2019-06-04 23:37:14 +0900

    wip: list

--- src/test/scala/data_structures/ListSpec.scala
+++ src/test/scala/data_structures/ListSpec.scala
@@ -5,4 +5,12 @@ class ListSuite extends FunSuite {
   test("sum") {
     assert(List.sum(List(1, 2, 3, 4)) === 10)
   }
+
+  test("product") {
+    assert(List.product(List(1.0, 2.0, 3.0, 4.0)) === 24.0)
+  }
+
+  // test("product") {
+  //   assert(List.product(List(1.0, 2.0, 3.0, 4.0)) === 24.0)
+  // }
 }
