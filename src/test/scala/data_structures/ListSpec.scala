bb9a08dcef898b364dcf2c0b5a761d8cf22c4221
KentFujii <kent.where.the.light.is@gmail.com>
2019-06-06 00:27:21 +0900

    wip: data_structures

--- src/test/scala/data_structures/ListSpec.scala
+++ src/test/scala/data_structures/ListSpec.scala
@@ -1,16 +1,25 @@
 package data_structures
 import org.scalatest._
 
-class ListSuite extends FunSuite {
-  test("sum") {
-    assert(List.sum(List(1, 2, 3, 4)) === 10)
+class ListSuite extends FunSpec {
+  describe("sum") {
+    it("Nil") {
+      assert(List.sum(Nil) === 0)
+    }
+    it("List(1, 2, 3, 4)") {
+      assert(List.sum(List(1, 2, 3, 4)) === 10)
+    }
   }
 
-  test("product") {
-    assert(List.product(List(1.0, 2.0, 3.0, 4.0)) === 24.0)
+  describe("product") {
+    it("Nil") {
+      assert(List.product(Nil) === 1.0)
+    }
+    it("List(0.0, 1.0)") {
+      assert(List.product(List(0.0, 1.0)) === 0.0)
+    }
+    it("List(1.0, 2.0, 3.0, 4.0)") {
+      assert(List.product(List(1.0, 2.0, 3.0, 4.0)) === 24.0)
+    }
   }
-
-  // test("product") {
-  //   assert(List.product(List(1.0, 2.0, 3.0, 4.0)) === 24.0)
-  // }
 }
--- src/test/scala/getting_started/GettingStartedSpec.scala
+++ src/test/scala/getting_started/GettingStartedSpec.scala
@@ -1,13 +1,17 @@
 package getting_started
 import org.scalatest._
 
-class GettingStartedSpec extends FunSuite {
-  test("formatResult") {
-    assert(GettingStarted.formatResult("absolute value", 42, GettingStarted.abs) === "The absolute value of 42 is 42")
-    assert(GettingStarted.formatResult("factorial value", 7, GettingStarted.factorial) === "The factorial value of 7 is 5040")
+class GettingStartedSpec extends FunSpec {
+  describe("formatResult") {
+    it("absolute value") {
+      assert(GettingStarted.formatResult("absolute value", 42, GettingStarted.abs) === "The absolute value of 42 is 42")
+    }
+    it("factorial value") {
+      assert(GettingStarted.formatResult("factorial value", 7, GettingStarted.factorial) === "The factorial value of 7 is 5040")
+    }
   }
 
-  test("findFirst") {
+  describe("findFirst") {
     assert(GettingStarted.findFirst(Array(7, 9, 13), (x: Int) => x == 9) === 1)
   }
 }
